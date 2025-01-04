8071017517d1cc14b150d18832f7765308a6501a
Victor Leonam <169005086+VicLeonam1@users.noreply.github.com>
2025-01-04 12:46:18 -0300

    iniciando curso de react

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ola mundo</h1>
+      <p>meu primeiro App</p>
+    </div>
+  );
+}
+
+export default App;
